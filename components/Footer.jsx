3b60e82edf7dd0a7c46a814140d4d4b9afd5801b
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-06-27 21:06:41 -0600

    feat(app): Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    
+    /* Hello */
     <footer className='w-full flex items-center'>
     <div className="mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="border-t border-gray-200 py-8 text-center text-sm text-gray-500 sm:text-left">
